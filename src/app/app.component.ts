8dd87e8d1a493984c98c857691e0b05c0066766e
DangSauQuaKhu <97039865+DangSauQuaKhu@users.noreply.github.com>
2023-04-07 10:06:55 +0700

    todoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,25 @@ import { Component } from '@angular/core';
   <h1>
     Welcome to {{ title }}!
   </h1>
-
+  <h1 *ngIf="userLoggedIn">Welcome guys!</h1>
   <app-input-button-unit></app-input-button-unit>
+  <ul>
+  <li *ngFor="let TodoItem of todoList" >
+  {{TodoItem.title}}
+  </li>
+  </ul>
 `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
+  userLoggedIn = 1;
 }
